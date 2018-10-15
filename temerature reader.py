af806caacf68c239f8e1e0873a0a65788a23df95
u1850532 <u1850532@unimail.hud.ac.uk>
2018-10-15 14:38:40 +0100

    my work

--- temerature reader.py
+++ temerature reader.py
@@ -1,16 +1,19 @@
 def temperature():
+    average = 0
     list_of_temps =[]
-    temp_to_list = input("please enter a temperature(remember .00);\n")
+    temp_to_list = input("please enter a temperature;\n")
+    average += int(temp_to_list)
     list_of_temps.append(temp_to_list)
     while temp_to_list.isdigit():
-        temp_to_list = input("please enter a temperature(remember .00);\n")
+        temp_to_list = input("please enter a temperature;\n")
         if temp_to_list.isdigit():
-            int(temp_to_list)
+            average += int(temp_to_list)
+            print(average)
             list_of_temps.append(temp_to_list)
-        print(list_of_temps)
-    int(list_of_temps[0])
-    print(max(list_of_temps))
-    print(min(list_of_temps))
-    print(sum(list_of_temps) / int(len(list_of_temps)))
+    print("a non number was entered")
+    print("\n")
+    print("The maximum temperature you gave was:\n", max(list_of_temps))
+    print("The minimum temperature you gave was:\n", min(list_of_temps))
+    print("The average temperature you gave was:\n", average / int(len(list_of_temps)))
 
 temperature()
