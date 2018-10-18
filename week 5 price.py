0a4efaa5167262f57c7a64d36d66f4a334ca2548
Mattyr3200 <u1850532@unimail.hud.ac.uk>
2018-10-18 17:21:39 +0100

    my work

--- week 5 price.py
+++ week 5 price.py
@@ -1,10 +1,11 @@
 def price():
-    l = ()
+    prices = []
+    total_price = 0
     for x in range(5):
         number = input("what is the price:\n")
-        l = (number[:1])
-
-    print(sum(l)/len(l))
-    print(max(l))
-    print(min(l))
+        prices.append(number[:2])
+        total_price += int(number[:2])
+    print("the average of the prices you have given is:\n", total_price / len(prices))
+    print("the maximum of the prices you have given is:\n", max(prices))
+    print("the minimum of the prices you have given is:\n", min(prices))
 price()
